7410a090f497c338aba8b99b3bdba2f025e5d570
Daniel Saldaña <danielsaldana@Daniels-MacBook-Pro.local>
2019-10-07 09:35:41 -0500

    Base app

---
+++ Get My Grade/Get My Grade/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Get My Grade
+//
+//  Created by Daniel Saldaña on 10/7/19.
+//  Copyright © 2019 Daniel Saldaña. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
