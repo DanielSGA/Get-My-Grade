03b362620025f1040b4bc887efa74732a3f5f476
jorgeramos2 <jorge.ramos1996@gmail.com>
2019-11-20 19:17:22 -0600

    teclado

--- Get-My-Grade-master 2/get my grade modificar 2/GetMyGrade 2 2 3/GetMyGrade/ViewControllerAgregaActividad.swift
+++ Get-My-Grade-master 2/get my grade modificar 2/GetMyGrade 2 2 3/GetMyGrade/ViewControllerAgregaActividad.swift
@@ -13,11 +13,13 @@ func guardaActividades()->Void
 }
 class ViewControllerAgregaActividad: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var tfNombre: UITextField!
     @IBOutlet weak var tfCalificacion: UITextField!
     var delegado: protocoloAgregaActividad!
     var idCategoria: Int!
-    
+    var scrollOffset : CGFloat = 0
+    var distance : CGFloat = 0
     @IBAction func quitaTeclado() {
         view.endEditing(true)
     }
@@ -47,7 +49,45 @@ return UIInterfaceOrientationMask.landscape
 override var shouldAutorotate: Bool {
 return false
 }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
+    }
+        @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
 
+            var safeArea = self.view.frame
+            safeArea.size.height += scrollView.contentOffset.y
+            safeArea.size.height -= keyboardSize.height + (UIScreen.main.bounds.height*0.04) // Adjust buffer to your liking
+
+            // determine which UIView was selected and if it is covered by keyboard
+
+            let activeField: UIView? = [tfNombre,tfCalificacion].first { $0.isFirstResponder }
+            if let activeField = activeField {
+                if safeArea.contains(CGPoint(x: 0, y: activeField.frame.maxY)) {
+                    print("No need to Scroll")
+                    return
+                } else {
+                    distance = activeField.frame.maxY - safeArea.size.height
+                    scrollOffset = scrollView.contentOffset.y
+                    self.scrollView.setContentOffset(CGPoint(x: 0, y: scrollOffset + distance), animated: true)
+                }
+            }
+            // prevent scrolling while typing
+
+            scrollView.isScrollEnabled = false
+        }
+    }
+    @objc func keyboardWillHide(notification: NSNotification) {
+            if distance == 0 {
+                return
+            }
+            // return to origin scrollOffset
+            self.scrollView.setContentOffset(CGPoint(x: 0, y: scrollOffset), animated: true)
+            scrollOffset = 0
+            distance = 0
+            scrollView.isScrollEnabled = true
+    }
     /*
     // MARK: - Navigation
 
--- Get-My-Grade-master 2/get my grade modificar 2/GetMyGrade 2 2 3/GetMyGrade/ViewControllerAgregaCategoria.swift
+++ Get-My-Grade-master 2/get my grade modificar 2/GetMyGrade 2 2 3/GetMyGrade/ViewControllerAgregaCategoria.swift
@@ -13,11 +13,13 @@ protocol protocoloAgregaCategoria{
 }
 class ViewControllerAgregaCategoria: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var tfNombre: UITextField!
     @IBOutlet weak var tfPorcentaje: UITextField!
     var delegado: protocoloAgregaCategoria!
     var idMateria : Int!
-    
+    var scrollOffset : CGFloat = 0
+    var distance : CGFloat = 0
     @IBAction func quitaTeclado() {
         view.endEditing(true)
     }
@@ -48,7 +50,45 @@ class ViewControllerAgregaCategoria: UIViewController {
     override var shouldAutorotate: Bool {
     return false
     }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
+    }
+        @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
 
+            var safeArea = self.view.frame
+            safeArea.size.height += scrollView.contentOffset.y
+            safeArea.size.height -= keyboardSize.height + (UIScreen.main.bounds.height*0.04) // Adjust buffer to your liking
+
+            // determine which UIView was selected and if it is covered by keyboard
+
+            let activeField: UIView? = [tfNombre,tfPorcentaje].first { $0.isFirstResponder }
+            if let activeField = activeField {
+                if safeArea.contains(CGPoint(x: 0, y: activeField.frame.maxY)) {
+                    print("No need to Scroll")
+                    return
+                } else {
+                    distance = activeField.frame.maxY - safeArea.size.height
+                    scrollOffset = scrollView.contentOffset.y
+                    self.scrollView.setContentOffset(CGPoint(x: 0, y: scrollOffset + distance), animated: true)
+                }
+            }
+            // prevent scrolling while typing
+
+            scrollView.isScrollEnabled = false
+        }
+    }
+    @objc func keyboardWillHide(notification: NSNotification) {
+            if distance == 0 {
+                return
+            }
+            // return to origin scrollOffset
+            self.scrollView.setContentOffset(CGPoint(x: 0, y: scrollOffset), animated: true)
+            scrollOffset = 0
+            distance = 0
+            scrollView.isScrollEnabled = true
+    }
     /*
     // MARK: - Navigation
 
