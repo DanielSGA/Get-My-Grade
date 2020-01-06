978d2f8fd40f10fd3bc4bf9105390da6826493c6
jorgeramos2 <jorge.ramos1996@gmail.com>
2020-01-06 16:55:21 -0600

    font

--- GetMyGrade/AppDelegate.swift
+++ GetMyGrade/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.font: UIFont(name: "ChalkboardSE-Light", size: 20.0)!]
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.font: UIFont(name: "KohinoorDevanagari-Light", size: 20.0)!]
         return true
     }
 
